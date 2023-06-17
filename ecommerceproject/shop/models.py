6888ac0ef3e48881f19260db41087de190c878e8
vademmal <vineethavademmal@gmail.com>
2023-06-16 23:50:03 -0700

    added

---
+++ ecommerceproject/shop/models.py
@@ -0,0 +1,82 @@
+from django.db import models
+from django.urls import reverse
+
+# Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=250,unique=True)
+    slug=models.SlugField(max_length=250,unique=True)
+    description=models.TextField(blank=True)
+    image=models.ImageField(upload_to='category',blank=True)
+
+    class Meta:
+        ordering=('name',)
+        verbose_name='category'
+        verbose_name_plural='categories'
+
+    def get_url(self):
+        return reverse('shop:product_by_category',args=[self.slug])
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+class Product(models.Model):
+    name=models.CharField(max_length=250,unique=True)
+    slug=models.SlugField(max_length=250,unique=True)
+    description=models.TextField(blank=True)
+    price=models.DecimalField(max_digits=10,decimal_places=2)
+    category=models.ForeignKey(Category, on_delete=models.CASCADE)
+    image=models.ImageField(upload_to='product',blank=True)
+    stock=models.IntegerField()
+    available=models.BooleanField(default=True)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('shop:prodCatdetail', args=[self.category.slug,self.slug])
+
+    class Meta:
+        ordering=('name',)
+        verbose_name='product'
+        verbose_name_plural='products'
+
+
+    def __str__(self):
+        return '{}'.format(self.name)
+
+
+
+
+
+
+
+
+
+
+
+# from django.db import models
+# from shop.models import Product
+#
+# # Create your models here.
+#
+# class Cart(models.Model):
+#      card_id=models.CharField(max_length=250,blank=True)
+#      date_added=models.DateField(auto_now_add=True)
+#
+#      class Meta:
+#          db_table='Cart'
+#          ordering=['date_added']
+#      def __str__(self):
+#         return  self.cart_id
+#
+# class CartItem(models.Model):
+#     product=models.ForeignKey(Product,on_delete=models.CASCADE)
+#     cart=models.ForeignKey(Cart, on_delete=models.CASCADE)
+#     quantity=models.IntegerField()
+#     active=models.BooleanField(default=True)
+#     class Meta:
+#         db_table='CartItem'
+#     def sub_total(self):
+#         return self.product.price * self.quantity
+#     def __str__(self):
+#         return  self.product
